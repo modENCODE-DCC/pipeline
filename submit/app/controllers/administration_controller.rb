488607d6be8595de28df442659c67b62904248ed
yostinso <yostinso>
2008-09-18 16:48:25 +0000

    Added sections to show queued and running commands

--- submit/app/controllers/administration_controller.rb
+++ submit/app/controllers/administration_controller.rb
@@ -10,6 +10,10 @@ class AdministrationController < ApplicationController
       File.stat(project_dir) # Make sure any automounting that needs to be done is done
     end
     @files = [ project_dir ]
+
+    @all_queued_commands = Command.find_all_by_status(Command::Status::QUEUED).sort { |c1, c2| c1.queue_position <=> c2.queue_position }
+
+    @active_commands = Command.all.find_all { |c| c.status =~ /ing$/ }.sort { |c1, c2| c1.queue_position <=> c2.queue_position }
   end
 
   def admin_required
