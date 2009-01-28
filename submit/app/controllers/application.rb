bb9bf739f419b61add74ba29f09ed1c19084d18b
yostinso <yostinso>
2009-01-28 00:59:17 +0000

    Show 404 error instead of Unknown action

--- submit/app/controllers/application.rb
+++ submit/app/controllers/application.rb
@@ -8,5 +8,10 @@ class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   #session :session_key => '_encPipeline_session_id'
   session :session_key => "_#{ActiveRecord::Base.configurations[RAILS_ENV]['database']}_session_id"
+  rescue_from ActionController::UnknownAction, :with => :action_not_found
+
+  def action_not_found
+    redirect_to :controller => "404.html"
+  end
 
 end
