da2ba618cad5b091884ddeb499bb82c38c7b0155
yostinso <yostinso>
2009-03-16 21:30:42 +0000

    Fix sorting by releasedness

--- submit/app/controllers/pipeline_controller.rb
+++ submit/app/controllers/pipeline_controller.rb
@@ -1736,12 +1736,12 @@ private
       sorts = session[:sort_list].sort_by { |column, sortby| sortby[1] }.reverse.map { |column, sortby| column }
       @projects = @projects.sort { |p1, p2|
         p1_attrs = sorts.map { |col| 
-          sort_attr = (session[:sort_list][col][0] == 'backward') ?  p2.attributes[col] : p1.attributes[col]
+          sort_attr = (session[:sort_list][col][0] == 'backward') ?  p2.send(col) : p1.send(col)
           sort_attr = Project::Status::state_position(sort_attr) if col == "status"
           sort_attr
         } << p1.id
         p2_attrs = sorts.map { |col| 
-          sort_attr = (session[:sort_list][col][0] == 'backward') ?  p1.attributes[col] : p2.attributes[col] 
+          sort_attr = (session[:sort_list][col][0] == 'backward') ?  p1.send(col) : p2.send(col) 
           sort_attr = Project::Status::state_position(sort_attr) if col == "status"
           sort_attr
         } << p2.id
