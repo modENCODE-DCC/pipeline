4957dd71d7f130520d83990cbe7626c52b53f96b
yostinso <yostinso>
2009-04-09 00:46:16 +0000

    Watch out for uploads without project_archives, as when inserting a README when one is missing

--- submit/app/controllers/upload_controller.rb
+++ submit/app/controllers/upload_controller.rb
@@ -15,11 +15,11 @@ class UploadController < CommandController
   def do_after(options = {})
     # Update project status and clean up project archive
     project_archive = command_object.project.project_archives.all.find { |pa| File.basename(pa.file_name) == File.basename(options[:destfile]) }
-    if command_object.status == Upload::Status::UPLOAD_FAILED then
-      project_archive.destroy if project_archive
-      return false
-    else
-      if project_archive then
+    if project_archive then
+      if command_object.status == Upload::Status::UPLOAD_FAILED then
+        project_archive.destroy if project_archive
+        return false
+      else
         project_archive.file_size = File.size(options[:destfile])
         project_archive.status = ProjectArchive::Status::NOT_EXPANDED
         project_archive.is_active = true
