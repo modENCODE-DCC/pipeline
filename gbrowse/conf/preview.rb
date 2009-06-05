01c22edd39183bf2472435d98772555fc385241d
yostinso <yostinso>
2009-06-05 00:55:21 +0000

    Fix display of multiple organisms

--- gbrowse/conf/preview.rb
+++ gbrowse/conf/preview.rb
@@ -5,10 +5,6 @@ require 'socket'
 require 'rubygems'
 require '../../../submit/config/environment'
 
-File.open("../../conf/modencode_fly.conf") { |f|
-  puts f.read
-}
-
 path_prefix = "/srv/www/data/pipeline_public"
 if Socket.gethostname == "smaug" then
   path_prefix = "/var/www/data/pipeline"
@@ -19,13 +15,23 @@ if ARGV[0] && ARGV[0] !~ /worm|fly/ then
   preview_conf = File.join(path_prefix, ARGV[0], "/browser/", "#{ARGV[0]}.conf");
   if File.exist?(preview_conf) then
     File.open(preview_conf) { |f|
-      puts f.read
+      conf_text = f.read
+      organism = (conf_text =~ /##species worm/) ? "worm" : "fly"
+      File.open("../../conf/modencode_#{organism}.conf") { |f2|
+        puts f2.read
+      }
+      puts conf_text
     }
   end
 else
   # Show all previews
   organism = ARGV[0]
   organism = "fly" unless organism
+
+  File.open("../../conf/modencode_#{organism}.conf") { |f|
+    puts f.read
+  }
+
   conf_name = "../../conf/tmp_conf/modencode_preview_#{organism}.conf"
   if File.exist?(conf_name) && (Time.now - File.mtime(conf_name)) < 600 then
     File.open(conf_name) { |f|
