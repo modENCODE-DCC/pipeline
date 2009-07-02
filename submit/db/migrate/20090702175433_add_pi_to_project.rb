3da3bc3e0594573de6c6bd51bf3f27d0db7d4d56
yostinso <yostinso>
2009-07-02 19:25:14 +0000

    Fix migration for setting pi

--- submit/db/migrate/20090702175433_add_pi_to_project.rb
+++ submit/db/migrate/20090702175433_add_pi_to_project.rb
@@ -2,7 +2,7 @@ class AddPiToProject < ActiveRecord::Migration
   def self.up
     add_column :projects, :pi, :string
     Project.all.each { |p|
-      p.lab = p.user.pi unless p.user.nil?
+      p.pi = p.user.pi unless p.user.nil?
       p.save
     }
   end
