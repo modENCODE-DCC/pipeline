6a706c2ca0b75385e6f5e7ecb8c285e18e5b0278
yostinso <yostinso>
2009-09-03 21:58:38 +0000

    Workaround a horrible Postgres bug that means bytea-escaped data doesn't get returned properly if it's an 87-char string starting with 3 digits

--- submit/app/models/track_stanza.rb
+++ submit/app/models/track_stanza.rb
@@ -1,3 +1,4 @@
+require 'base64'
 class TrackStanza < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
@@ -6,9 +7,16 @@ class TrackStanza < ActiveRecord::Base
   validates_presence_of :marshaled_stanza
 
   def stanza
-    return Marshal.restore(self.marshaled_stanza)
+    s = self.marshaled_stanza
+    if s[0..10] == "b64_header" then
+      # Remove (new) header
+      s = s[11..-1]
+      return Base64.decode64(Marshal.restore(s))
+    else
+      return Marshal.restore(s)
+    end
   end
   def stanza=(newstanza)
-    self.marshaled_stanza = Marshal.dump(newstanza)
+    self.marshaled_stanza = "b64_header" + Base64.encode64(Marshal.dump(newstanza))
   end
 end
