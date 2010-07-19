9e6c2ea7461c0313ac44f85f8793c1af9a2d465c
ellen <ellen>
2010-07-19 22:48:36 +0000

    aaaand, fixed a more important bug where I forgot to check for empty files. Whoops.

--- submit/script/BatchUpdateAllSignatures.rb
+++ submit/script/BatchUpdateAllSignatures.rb
@@ -12,6 +12,7 @@ basepath = File.expand_path("#{ActiveRecord::Base.configurations[RAILS_ENV]['upl
 
 # Update signatures of all project archives
 num_archive_sigs = 0
+skipped_archives = 0
 ProjectArchive.all.each{|pa|
   
   # get file of archive
@@ -19,9 +20,17 @@ ProjectArchive.all.each{|pa|
   unless File.exist? archivefile then
     puts "#{pa.id} - This archive has no file #{archivefile} - skipping"
     archivesig = nil
+    skipped_archives += 1
   else
-  archivesig = Digest::MD5.hexdigest(File.read(archivefile, BYTES_TO_MD5))
-  num_archive_sigs += 1
+  file_to_process = File.read(archivefile, BYTES_TO_MD5)
+  if file_to_process.nil? then
+    puts "#{pa.id} - File #{archivefile} exists but is empty - skipping"
+    archivesig = nil
+    skipped_archives += 1
+  else
+  	archivesig = Digest::MD5.hexdigest(file_to_process)
+  	num_archive_sigs += 1
+  end
   end
   pa.signature = archivesig
   pa.save
@@ -39,26 +48,36 @@ ProjectFile.all.each{|pf|
   filesig = nil
   if pf.is_overwritten then
     puts "#{pf.id} - this file is overwritten -- skipping"
+    skipped_files = 0
   else
     filearchive = pf.project_archive
     if filearchive.nil? then
       puts "#{pf.id} is in archive #{pf.project_archive_id} but that archive's nil! skipping."
+      skipped_files += 1  
     else
       fileproject = filearchive.project
       if fileproject.nil? then
         puts "#{pf.id}'s archive is in project #{filearchive.project_id} but that project's nil! skipping."
+        skipped_files += 1
       else
         filepath = File.join(basepath, fileproject.id.to_s, "extracted", pf.file_name)
         unless File.exists? filepath then
           puts "#{pf.id}'s file #{pf.file_name} can't be found in its project #{fileproject.id}! Skipping"
+          skipped_files += 1
         else
-          # the file is there! hurrah!
-          filesig = Digest::MD5.hexdigest(File.read(filepath, BYTES_TO_MD5))
-          num_file_sigs += 1
+	  file_to_process = File.read(filepath, BYTES_TO_MD5)
+          if file_to_process.nil? then
+            puts "#{pf.id} - File #{filepath} exists but is empty - skipping"
+            skipped_files += 1
+          else  
+            # the file is there! hurrah!
+            filesig = Digest::MD5.hexdigest(File.read(filepath, BYTES_TO_MD5))
+            num_file_sigs += 1
+          end
         end
-     end
-   end
- end
+      end
+    end
+  end
  
   pf.signature = filesig
   pf.save
@@ -67,4 +86,4 @@ ProjectFile.all.each{|pf|
 }
 elapsedtime = Time.now - starttime
 
-puts "Generated signatures for #{num_archive_sigs} archives and #{num_file_sigs} files in #{elapsedtime} seconds."
+puts "Generated signatures for #{num_archive_sigs} archives and #{num_file_sigs} files in #{elapsedtime} seconds. Skipped #{skipped_archives} archives and #{skipped_files} files."
