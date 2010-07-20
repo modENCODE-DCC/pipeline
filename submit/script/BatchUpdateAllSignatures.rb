94a374c2f8b106a7f055d0be10e68007af5b7b3d
ellen <ellen>
2010-07-20 19:30:56 +0000

    hopefully I won't have to muck around with this thing anymore.

--- submit/script/BatchUpdateAllSignatures.rb
+++ submit/script/BatchUpdateAllSignatures.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/ruby
+
+# It is recommended to run this as sudo -u www-data or whatever the rails user actually is.
+# Note that currently it skips anything that already has a signature
 require 'digest/md5'
 require File.dirname(__FILE__) + '/../config/environment'
 require 'find'
@@ -14,7 +17,12 @@ basepath = File.expand_path("#{ActiveRecord::Base.configurations[RAILS_ENV]['upl
 num_archive_sigs = 0
 skipped_archives = 0
 ProjectArchive.all.each{|pa|
-  
+  # Optional - skip it if it already has a signature
+  if pa.signature then
+    puts ":#{pa.id} has sig #{pa.signature}"
+    next
+  end
+  print "#{pa.id}: "
   # get file of archive
   archivefile = File.join(basepath, pa.project_id.to_s, pa.file_name)
   unless File.exist? archivefile then
@@ -37,6 +45,12 @@ ProjectArchive.all.each{|pa|
   puts "Archive #{pa.id}: added signature #{archivesig}" unless archivesig.nil?
   
 }
+# Because it crashed, skip the ones that already ran for better fasterness.
+# This is optional and useful!
+#already_ran = File.readlines("files_to_skip_for_batch")
+#done_files = {}
+#already_ran.each{|ar| done_files[ar.chomp.to_i] = true }
+
 
 # also do projectFiles
 
@@ -44,11 +58,22 @@ ProjectArchive.all.each{|pa|
 # file_name = full path to it starting from expanded folder
 # if it's overwritten, don't do it!
 num_file_sigs = 0
+skipped_files = 0
 ProjectFile.all.each{|pf|
+#  if done_files[pf.id] then
+#    puts ":#{pf.id} already ran"
+#    next
+#  end
+  # Optional - skip it if it already has a signature
+  if pf.signature then
+    puts ":#{pf.id} has sig #{pf.signature}"
+    next
+  end
+  print "#{pf.id}: "
   filesig = nil
   if pf.is_overwritten then
     puts "#{pf.id} - this file is overwritten -- skipping"
-    skipped_files = 0
+    skipped_files += 1
   else
     filearchive = pf.project_archive
     if filearchive.nil? then
