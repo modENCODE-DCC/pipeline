48959c343abbd92b8dfe81c38deada0a3aade0a6
ellen <ellen>
2011-08-23 22:32:24 +0000

    Added google analytics tracking javascrit

--- submit/app/helpers/application_helper.rb
+++ submit/app/helpers/application_helper.rb
@@ -4,6 +4,24 @@ module ApplicationHelper
   # Usage:
   # <%=google_pie_chart([["Very Liberal", 8], ["Liberal", 22], ["Moderate", 9], ["Conservative", 4], ["Very Conservative", 1]], :width => 626, :height => 300) %>
 
+  def google_analytics_js
+    <<-EOS
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-25200492-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
+    EOS
+  end
+
   def google_horiz_bar_chart(d, options = {})
     options[:width] ||= 300
     options[:height] ||= "#{options[:data].length*30+60}"
